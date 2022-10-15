0dae90b610d39be1eb018828d76cd555afcbf217
Sore09 <soreoluwa09@gmail.com>
2022-10-14 17:44:09 -0700

    task 6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 
+
 /**
- * main - Entry point
- *
+ * main - Entry point 
  * Return: Always 0
  */
 
 int main(void)
+
 {
 	printf("Size of a char: %1u byte(s)\n", sizeof(char));
 	printf("Size of a int: %1u byte(s)\n", sizeof(int));  
