35f3318856aed40d1d9ed14ac36f93343fe44266
Sore09 <soreoluwa09@gmail.com>
2022-10-18 16:34:46 -0700

    task 0

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -5,11 +5,10 @@
 
 /* betty style doc for function main goes there */
 /**
-* main - Entry point
-*
-* Return: Always 0 (success)
-*/
-
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
 int main(void)
 {
 	int n;
