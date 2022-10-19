04ebbac1ff87beb74025555c6fae1564d3d169a4
Sore09 <soreoluwa09@gmail.com>
2022-10-19 16:45:58 -0700

    task 8

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -5,15 +5,13 @@
  * @n: the passed args
  * Return: the last digit
  */
-
 int print_last_digit(int n)
 {
-	int x = n % 10;
+	int x;
 
-	if (x < 0)
-		x *= -1;
-
+	if (n < 0)
+	    n = -n;
+	x = n % 10;
 	_putchar(x + '0');
-
-	return (0);
+	return (x);
 }
