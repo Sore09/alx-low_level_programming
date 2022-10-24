63a6c283a4878a47aadb6295515f79fe5673dca5
Sore09 <soreoluwa09@gmail.com>
2022-10-24 16:44:49 -0700

    task 6

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * puts2 - prints one character out of two
+ * @str: input
+ * Return: print
+ */
+
+void puts(char *str)
+{
+	int i = 0;
+
+	for (; str[i] != '\0'; i++)
+	{
+		if ((i % 2) == 0)
+			_putchar(str[1]);
+		else
+			continue;
+	}
+	_putchar('\n');
+}
