e2d39a84cc08ca08919c91c15cd80a36933082c5
Sore09 <soreoluwa09@gmail.com>
2022-11-02 17:31:46 -0700

    task 5

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -12,3 +12,18 @@ int _sqrt_recursion(int n)
 {
 	return (_sqrt(n, 1));
 }
+
+/**
+ * _sqrt -> recursive square root
+ * @n: number
+ * @i: iteration
+ * Return: a number
+ */
+int _sqrt(int n, int i)
+{
+	if (square > n)
+		return (-1);
+	if (square == n)
+		return (i);
+	return (_sqrt(n, i + 1));
+}
