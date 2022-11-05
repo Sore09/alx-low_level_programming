40f28bf594f0e7082b225ba7a1ecd69fb8e1615f
Sore09 <soreoluwa09@gmail.com>
2022-11-04 18:18:56 -0700

    task 3

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -10,11 +10,11 @@
 
 int main(int argc, char *argv[])
 {
-	int result, num1, num2;
+	int result = 0, num1, num2;
 
 	if (argc != 3)
 	{
-		printf("s\n", "Error");
+		printf("%s\n", "Error");
 		return (1);
 	}
 	num1 = atoi(argv[1]);
